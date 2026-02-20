3b29d4abb0b5f593ff955c244725528e3df605e3
MrBitasmblTester2 <lulu2214@proton.me>
2026-02-20 11:40:32 +0400

    Add main.go

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,3 @@
+package main
+import "net/http"
+func main(){mux:=http.NewServeMux();mux.HandleFunc("/health",func(w http.ResponseWriter,r *http.Request){w.Write([]byte("ok"))});http.ListenAndServe(":8080",mux)}

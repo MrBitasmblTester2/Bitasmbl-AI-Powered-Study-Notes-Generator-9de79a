4b3a292f6a9a87b8ca443fa03b90afa56a31904f
MrBitasmblTester2 <lulu2214@proton.me>
2026-02-20 11:40:33 +0400

    Add handlers.go

---
+++ backend/internal/http/handlers.go
@@ -0,0 +1,5 @@
+package http
+import "net/http"
+func UploadMaterial(w http.ResponseWriter,r *http.Request){}
+func GenerateNotes(w http.ResponseWriter,r *http.Request){}
+func GetNotes(w http.ResponseWriter,r *http.Request){}
